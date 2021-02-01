ba8d07e7b8421cce849b014fb72ce22498a960e5
Richard <richardsouzarodrigues555@gmail.com>
2021-02-01 17:44:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<img src='./assets/logo.svg' alt='Logo' id='logo'/>
+Projeto de finanças da Maratona Discover da <a href='https://github.com/rocketseat-education' target="_blank">Rocketseat</a> feito em JavaScript puro
+<hr>
+
+<div float='left'>
+  <img src='./assets/screenshots/Screenshot-pc.png' alt='Layout Screenshot Full size' width='550em'/>
+
+  <img src='./assets/screenshots/Screenshot-mobile.png' alt='Layout Screenshot Mobile' width='250em'/>
+</div>
+<hr>
+
+<h2>Tecnologies:</h2>
+<ul>
+  <li>JavaScript</li>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>LocalStorage - to storage the transaction data</li>
+</ul>
---
+++ script.js
@@ -0,0 +1,200 @@
+const Modal = {
+	toggle() {
+		document
+			.querySelector('.modal-overlay')
+			.classList
+			.toggle('active')
+	}
+}
+
+const Utils = {
+	formatAmount(value) {
+		// Replace all ',' and '.' to nothing
+		value = Number(value.replace(/\,\./g, '')) * 100
+
+		return value
+	},
+
+	formatDate(value) {
+		const dateArray = value.split('-')
+
+		return `${dateArray[2]}/${dateArray[1]}/${dateArray[0]}`
+	},
+
+	formatCurrency(value) {
+		// Put '-' signal if the value is negative
+		const signal = Number(value) < 0 ? '-' : ''
+
+		// Remove all values that are not numbers
+		value = String(value).replace(/\D/g, '')
+
+		value = Number(value) / 100
+
+		// Format to BRL currency
+		value = value.toLocaleString('pt-BR', {
+			style: 'currency',
+			currency: 'BRL'
+		})
+
+		return signal + value
+	}
+}
+
+const Balance = {
+	incomes() {
+		let income = 0
+		transactions.forEach(transaction => {
+			if (transaction.amount > 0) {
+				income += transaction.amount
+			}
+		})
+		return income
+	},
+
+	expenses() {
+		let expense = 0
+		transactions.forEach(transaction => {
+			if (transaction.amount < 0) {
+				expense += transaction.amount
+			}
+		})
+		return expense
+	}, 
+
+	total() {
+		return Balance.incomes() + Balance.expenses()
+	}
+}
+
+function updateBalance() {
+	document
+		.getElementById('income-display')
+		.innerHTML = Utils.formatCurrency(Balance.incomes())
+	document
+		.getElementById('expense-display')
+		.innerHTML = Utils.formatCurrency(Balance.expenses())
+	document
+		.getElementById('total-display')
+		.innerHTML = Utils.formatCurrency(Balance.total())
+}
+
+const Storage = {
+	set() {
+
+		return localStorage.setItem('transactions', JSON.stringify(transactions))
+	},
+
+	get() {
+		return JSON.parse(localStorage.getItem('transactions')) || []
+	}
+}
+
+const transactions = Storage.get()
+
+
+const Form = {
+
+	formatValues() {
+		return {
+
+			amount: Utils.formatAmount(document.querySelector('input#amount').value),
+			date: Utils.formatDate(document.querySelector('input#date').value)
+		
+		}
+	},
+
+	getValues() {
+		const {amount, date} = Form.formatValues()
+
+		return {
+			description: document.querySelector('input#description').value,
+			amount,
+			date
+		}
+	},
+
+	submit(event) {
+		event.preventDefault()
+
+		const {description, amount, date} = Form.getValues()
+
+		// Validate input fields
+		if (document.querySelector('input#description').value === '' ||
+			document.querySelector('input#amount').value === '' ||
+			document.querySelector('input#date').value === '') return alert('Preencha todos os campos')
+
+		const data = {
+			description,
+			amount,
+			date
+		}	
+	 
+		Transaction.set(data)
+
+		// Clean input fields
+		document.querySelector('input#description').value = ''
+		document.querySelector('input#amount').value = ''
+		document.querySelector('input#date').value = ''
+	}
+}
+
+
+
+const Transaction = {
+	// Set transaction
+	set(value) {
+		transactions.push(value)
+
+		Storage.set()
+
+		Transaction.display()
+		Modal.toggle()
+	},
+
+	remove(index) {
+		transactions.splice(index, 1)
+
+		Storage.set()
+
+		Transaction.display()
+	},
+
+	// Update transactions
+	display() {
+
+		const tbody = document.getElementById('transactions-container')
+
+		// Cleanup to not show duplicates
+		tbody.innerHTML = ''
+		
+		transactions.forEach((value, index) => {
+			const tr = document.createElement('tr')
+
+
+			const amountClass = value.amount > 0 ? 'income' : 'expense'
+
+			const amount = Utils.formatCurrency(value.amount)
+
+			tr.innerHTML = `
+				<td class="description">${value.description}</td>
+                <td class=${amountClass}>${amount}</td>
+                <td>${value.date}</td>
+
+                <td>
+                    <img src="./assets/minus.svg" alt="Remover Transação"
+                    	style='cursor:pointer'
+                    	onclick='Transaction.remove(${index})'
+                    >
+                </td>
+			`
+
+			tbody.appendChild(tr) 
+		})
+
+		updateBalance()
+	}
+}
+
+Transaction.display()
+
+
